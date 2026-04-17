bbd7f4968c90931f3df6c91c25f289213644ef48
Crescent617 <lihuaru617@outlook.com>
2026-04-17 12:11:14 +0800

    fix: use GitHub Release for bottles

--- Formula/yomi.rb
+++ Formula/yomi.rb
@@ -8,6 +8,7 @@ class Yomi < Formula
   sha256 "5e91baa9ae0bbad908b17804faa3e2e68dbb69f96e51bf77cc248dc94345a5a8"
 
   bottle do
+    root_url "https://github.com/Crescent617/yomi/releases/download/v0.1.0"
     sha256 arm64_sequoia: "d2638d669c14cf24e10d0282c1969a5a4e76f1289a8306e77262a688a78564d9"
   end
 
