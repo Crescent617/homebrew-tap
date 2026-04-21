f2a43208a0d95e82d03d5179f0954ffcdd7161e2
Crescent617 <lihuaru617@outlook.com>
2026-04-21 15:48:38 +0800

    Update yomi to 0.1.8

--- Formula/yomi.rb
+++ Formula/yomi.rb
@@ -1,15 +1,15 @@
 class Yomi < Formula
   desc "AI coding assistant CLI built in Rust"
   homepage "https://github.com/Crescent617/yomi"
-  version "0.1.7"
+  version "0.1.8"
   license "MIT"
 
-  url "https://github.com/Crescent617/yomi/archive/refs/tags/v0.1.7.tar.gz"
-  sha256 "4238857dabc5bfd3ffb88656c2e5a38b6f699acc47149c52cfd14090329ff26c"
+  url "https://github.com/Crescent617/yomi/archive/refs/tags/v0.1.8.tar.gz"
+  sha256 "b24045c4bdef542b0cd1663e3bbecf326c9d12cb2de5d37bbe5607c37f1a411f"
 
   bottle do
-    root_url "https://github.com/Crescent617/yomi/releases/download/v0.1.7"
-    sha256 arm64_sequoia: "662d5bb376e89ed562c8479d2423f8db874b5c6fefff823eae2d7de34664157d"
+    root_url "https://github.com/Crescent617/yomi/releases/download/v0.1.8"
+    sha256 arm64_sequoia: "3a11779c5e0057b2c8ab54b899d73defa0e9ba2342433b9018f2bc3b93a8a1eb"
   end
 
   depends_on "rust" => :build
