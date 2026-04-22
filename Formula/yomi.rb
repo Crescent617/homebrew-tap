49b0698a4edc4dfea282c95fda513e1a48c84055
Crescent617 <lihuaru617@outlook.com>
2026-04-22 14:39:21 +0800

    Update yomi to 0.1.9

--- Formula/yomi.rb
+++ Formula/yomi.rb
@@ -5,11 +5,11 @@ class Yomi < Formula
   license "MIT"
 
   url "https://github.com/Crescent617/yomi/archive/refs/tags/v0.1.9.tar.gz"
-  sha256 "49686f194f974f034a60d5b0f6544c37729b8771fa7b0b1b2af4b32198e52f3f"
+  sha256 "5d912b9301ef4e449a542529b7e7e8bb8d853f8f0219ffe59f2bde958aa3593b"
 
   bottle do
     root_url "https://github.com/Crescent617/yomi/releases/download/v0.1.9"
-    sha256 arm64_sequoia: "d78750e40c2350a2e1e5c3eff6c67ce3d6d0bca7a5e37a8221f61520ce371f1d"
+    sha256 arm64_sequoia: "8e92fcec090ad0e7cdd5b657437be1e88aea4701df89f2ed7ea1ac414ae329e1"
   end
 
   depends_on "rust" => :build
