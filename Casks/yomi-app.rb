4f79dbb3254f9091396cdb501a027005bf31e271
Crescent617 <lihuaru617@outlook.com>
2026-06-03 15:01:23 +0800

    Bump yomi-app v0.1.0 (fixed DMG app name)

--- Casks/yomi-app.rb
+++ Casks/yomi-app.rb
@@ -1,6 +1,6 @@
 cask "yomi-app" do
   version "0.1.0"
-  sha256 "eb81bfe37ae5e54b31ecc7302db6c133b696f2813a414ab9935323c6f0e4960a"
+  sha256 "7fe24bffc517da99721be30aa3e0af2b17db73a4aaae0dbea8318872abf65935"
 
   url "https://github.com/Crescent617/yomi/releases/download/v#{version}/Yomi_#{version}_aarch64.dmg"
   name "Yomi"
