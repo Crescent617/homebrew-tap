2687dad4d62483e4d7bfc9eee997a622df7f8034
Crescent617 <lihuaru617@outlook.com>
2026-06-12 12:02:41 +0800

    Bump yomi-app to v0.2.38

--- Casks/yomi-app.rb
+++ Casks/yomi-app.rb
@@ -1,6 +1,6 @@
 cask "yomi-app" do
-  version "0.2.37"
-  sha256 "c76c7bf5237ebcef7b0267d4c09cf9f9f25034aa304490f5b9b3ba56cd8554d7"
+  version "0.2.38"
+  sha256 "b821b102453be3b80f844090b2f734a1d82e66f1fa3bde531392c87469463098"
 
   url "https://github.com/Crescent617/yomi/releases/download/v#{version}/Yomi_#{version}_aarch64.dmg"
   name "Yomi"
